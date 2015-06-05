9efe410994054988f0618466a2431fe45213c441
marzelwidmer <marzelwidmer@gmail.com>
2015-06-05 23:29:33 +0200

    disable security and tarter-data-rest add some JUnit Configurations

--- README.md
+++ README.md
@@ -9,3 +9,29 @@
 [travis-url]: https://travis-ci.org/marzelwidmer/logging-store
 [license-image]: http://img.shields.io/:license-Apache2.0-blue.svg?style=flat-square
 [license-url]: LICENSE
+
+
+
+# POST a Log Message
+```
+curl -X POST  -H "Accept: Application/json" -H "Content-Type: application/json" http://localhost:8080/log -d '{
+  "clientApplikation":"SPA1",
+  "clientVersion":"1.0.1",
+  "correlationId": "11212",
+  "debugInformation":"Hello World",
+  "faultMessage":"Message Hello World",
+  "faultCode":"289-36",
+  "faultType":"SYSTEM_UMGEBUNG",
+  "severity":"DEBUG"
+}'
+```
+
+# Start App with develpment profile
+```
+  java -jar  --spring.profiles.active=development target/
+```
+
+# Shutdown 
+```
+  curl -X POST localhost:9000/shutdown
+```
---
+++ src/test/java/ch/keepcalm/web/controller/LoggingRestControllerTest.java
@@ -0,0 +1,69 @@
+package ch.keepcalm.web.controller;
+
+import ch.keepcalm.web.LoggingStoreApplication;
+import ch.keepcalm.web.domain.LoggingStore;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by marcelwidmer on 01/06/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LoggingStoreApplication.class)
+@ActiveProfiles("test")
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class LoggingRestControllerTest {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://127.0.0.1:" + port + "/");
+        template = new TestRestTemplate();
+    }
+
+
+    private LoggingStore getDefualtLoggingStoreObject() {
+        LoggingStore loggingStore = new LoggingStore();
+        loggingStore.setClientApplikation("SPA");
+        loggingStore.setClientVersion("1.0");
+        loggingStore.setCorrelationId("11212");
+        loggingStore.setDebugInformation("Hello World");
+        loggingStore.setFaultMessage("Message Hello World");
+        loggingStore.setFaultCode("289-36");
+        loggingStore.setFaultType("DATEN");
+        loggingStore.setSeverity("DEBUG");
+        return loggingStore;
+    }
+
+    @Test
+    public void testLogRestController() {
+        LoggingStore loggingStore = getDefualtLoggingStoreObject();
+        loggingStore.setClientApplikation("JunitApplicationID");
+        loggingStore.setClientVersion("JUnitClientVersion");
+        RestTemplate restTemplate = new RestTemplate();
+        String log = restTemplate.postForObject(base + "log", loggingStore, String.class);
+        assertNotNull(log);
+    }
+
+
+
+}

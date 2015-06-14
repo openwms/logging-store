328cacea64681fe3a1c392b955f4dc54d9466729
marzelwidmer <marzelwidmer@gmail.com>
2015-06-14 14:25:32 +0200

    cleanup angular stuff

--- src/main/resources/static/app/controller/controller.js
+++ src/main/resources/static/app/controller/controller.js
@@ -1,5 +1,5 @@
 angular.module('myApp.controllers', [])
-    .controller('LoggingStoreController', function ($log, $scope, $http, LoggingStoreClient, InfoClient, HealthClient, EnvClient, NgTableParams) {
+    .controller('LoggingStoreController', function ($log, $scope, $http, $filter, LoggingStoreClient, InfoClient, HealthClient, EnvClient, NgTableParams) {
         'use strict';
 
         $scope.sortType = 'id'; // set the default sort type
@@ -10,49 +10,30 @@ angular.module('myApp.controllers', [])
         // GET ALL
         $scope.loggingStore = LoggingStoreClient.query(); //fetch all
 
-        var storeLogs = [];
-
-        LoggingStoreClient.query(function (data) {
-            for (var i = 0, len = data.length; i < len; i++) {
+/*
+        var storeData = [];
+*/
+        $scope.dataset = LoggingStoreClient.query(function (response) {
+            /*for (var i = 0, len = response.length; i < len; i++) {
                 var loggingStore = new LoggingStore();
-                loggingStore.setId(data[i].loggingStore.id);
-                loggingStore.setTimestamp(data[i].loggingStore.timestamp);
-                loggingStore.setClientApplikation(data[i].loggingStore.clientApplikation);
-                loggingStore.setClientVersion(data[i].loggingStore.clientVersion);
-                storeLogs.push(loggingStore);
+                loggingStore.setId(response[i].loggingStore.id);
+                loggingStore.setTimestamp(response[i].loggingStore.timestamp);
+                loggingStore.setClientApplikation(response[i].loggingStore.clientApplikation);
+                loggingStore.setClientVersion(response[i].loggingStore.clientVersion);
+                storeData.push(loggingStore);
             }
-            /*            console.log(data[0].loggingStore.id);
-             console.log(data[0].loggingStore.timestamp);
-             console.log(data[0].loggingStore.clientApplikation);*/
-        });
-        $scope.loggingStoreData = storeLogs;
-
-        var data = [{name: "Moroni", age: 50},
-            {name: "Tiancum", age: 43},
-            {name: "Jacob", age: 27},
-            {name: "Nephi", age: 29},
-            {name: "Enos", age: 34},
-            {name: "Tiancum", age: 43},
-            {name: "Jacob", age: 27},
-            {name: "Nephi", age: 29},
-            {name: "Enos", age: 34},
-            {name: "Tiancum", age: 43},
-            {name: "Jacob", age: 27},
-            {name: "Nephi", age: 29},
-            {name: "Enos", age: 34},
-            {name: "Tiancum", age: 43},
-            {name: "Jacob", age: 27},
-            {name: "Nephi", age: 29},
-            {name: "Enos", age: 34}];
-
-        $scope.tableParams = new NgTableParams({
-            page: 1,            // show first page
-            count: 10           // count per page
-        }, {
-            total: data.length, // length of data
-            getData: function ($defer, params) {
-                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-            }
+            $scope.dataset = storeData;
+             */
+            $scope.tableParams = new NgTableParams({
+                page: 1,            // show first page
+                count: 5           // count per page
+            }, {
+                total: $scope.dataset.length, // length of data
+                getData: function($defer, params) {
+                    $defer.resolve($scope.dataset.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }
+            });
+            console.log('all store entries - ', $scope.storeEntries.length);
         });
 
 
@@ -60,12 +41,6 @@ angular.module('myApp.controllers', [])
             return $scope.loggingStore.length;
         };
 
-
-        // callback for ng-click 'deleteUser':
-        /* $scope.deleteLog = function (storeId) {
-         LoggingStoreClient.delete({id: storeId});
-         $scope.store = LoggingStoreClient.query();
-         };*/
 
         // get application info
         $scope.infoClient = InfoClient.get();
@@ -75,97 +50,3 @@ angular.module('myApp.controllers', [])
         $scope.envClient = EnvClient.get();
         $scope.envProfile = $scope.envClient.profiles;
     });
-
-
-/**
- * Constructor, with class name
- * loggingStore: {
- *     id: 1,
- *     timestamp: 1434192016307,
- *     clientApplikation: "ccb1",
- *     clientVersion: "1.2.3",
- *     debugInformation: "mymessage",
- *     faultCode: "289-333423322232-2323-23-23-32-32232332",
- *     faultMessage: "Hello maxi World SEARCH ME IN THE INFORAMTION calculate errror",
- *     faultType: "SYSTEM_UMGEBUNG",
- *     severity: "INFO",
- *     correlationId: "100004"
- *     },
- *     link: {
- *     href: "http://localhost:8080/services/rest/log/1"
- * },
- */
-function LoggingStore() {
-    "use strict";
-    // Public properties, assigned to the instance ('this')
-    this.id;
-    this.timestamp;
-    this.clientApplikation;
-    this.clientVersion;
-
-    return {
-        setId: function (id) {
-            this.id = id;
-        },
-        getId: function () {
-            return id;
-        },
-        setTimestamp: function (timestamp) {
-            this.timestamp = timestamp;
-        },
-        getTimestamp: function () {
-            return timestamp;
-        },
-        setClientApplikation: function (clientApplikation) {
-            this.clientApplikation = clientApplikation;
-        },
-        getClientApplikation: function () {
-            return clientApplikation;
-        },
-        setClientVersion: function (clientVersion) {
-            this.clientVersion = clientVersion;
-        },
-        getClientVersion: function () {
-            return clientVersion;
-        }
-    };
-}
-
-/**
- * Constructor, with class name
- */
-function User() {
-    "use strict";
-    // Public properties, assigned to the instance ('this')
-    this.id;
-    this.firstname;
-    this.lastname;
-    this.selfLink;
-
-    return {
-        setId: function (id) {
-            this.id = id;
-        },
-        getId: function () {
-            return id;
-        },
-        setFirstname: function (firstname) {
-            this.firstname = firstname;
-        },
-        getFirstname: function () {
-            return firstname;
-        },
-        setLastname: function (lastname) {
-            this.lastname = lastname;
-        },
-        getLastname: function () {
-            return lastname;
-        },
-        setSelfLink: function (link) {
-            this.selfLink = link;
-        },
-        getSelfLink: function () {
-            return selfLink;
-        }
-    };
-}

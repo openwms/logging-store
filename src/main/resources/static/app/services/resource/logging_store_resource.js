b364ee63599ad0ce5ac1ea74449d30a3f4b90618
marzelwidmer <marzelwidmer@gmail.com>
2015-06-14 22:09:02 +0200

    update ng-grid

--- src/main/resources/static/app/services/resource/logging_store_resource.js
+++ src/main/resources/static/app/services/resource/logging_store_resource.js
@@ -5,6 +5,31 @@ angular.module('myApp.services', [])
         return $resource("/services/rest/log/:id ", {id: '@_id'}, {
             update: {
                 method: 'PUT'
+            },query: {
+                method: 'GET',
+                isArray: true,
+                transformResponse: function(data, headersGetter) {
+                    var items = angular.fromJson(data);
+                    var models = [];
+                    angular.forEach(items, function(item) {
+                        var loggingStore = new LoggingStore();
+                        loggingStore.id = item.loggingStore.id;
+                        loggingStore.timestamp = item.loggingStore.timestamp;
+                        loggingStore.clientApplication = item.loggingStore.clientApplikation;
+                        loggingStore.clientVersion = item.loggingStore.clientVersion;
+                        loggingStore.detail = item.link.href;
+
+                        console.log("item: ", item.loggingStore.id);
+                        console.log("item: ", item.loggingStore.timestamp);
+                        console.log("item: ", item.loggingStore.clientApplikation);
+                        console.log("item: ", item.loggingStore.clientVersion);
+                        console.log("item: ", item.link.href);
+
+                        models.push(loggingStore);
+                    });
+                    return models;
+                }
             }
         });
     });
+

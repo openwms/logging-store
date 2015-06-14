3214cdbd9c3effa334e5ec133b04f16aa9f25bc1
marzelwidmer <marzelwidmer@gmail.com>
2015-06-14 23:07:03 +0200

    upate UI

--- src/main/resources/static/app/controller/controller.js
+++ src/main/resources/static/app/controller/controller.js
@@ -12,20 +12,26 @@ angular.module('myApp.controllers', [])
             data: 'loggingStore'
         };
 
-
-        $scope.findIndex = function(row) {
-            // find real row by comparing $$hashKey with entity in row
-            var rowIndex = -1;
-            var hash = row.entity.$$hashKey;
-            var data = $scope.gridOptions.data;     // original rows of data
-            for (var ndx = 0; ndx < data.length; ndx++) {
-                if (data[ndx].$$hashKey == hash) {
-                    rowIndex = ndx;
-                    break;
-                }
-            }
-            return rowIndex;
+        $scope.mySelections = [];
+        $scope.gridOptions = {
+            data: 'loggingStore',
+            columnDefs: [
+                {field: 'id', displayName: 'ID', width: 50, sortable: true},
+                {field: 'timestamp', displayName: 'Timestamp', width: "auto" , pinnable: true},
+                {field: 'clientApplication', displayName: 'Application', width: 50},
+                {field: 'clientVersion', displayName: 'Version', width: 50},
+                {field: 'debugInformation', displayName: 'Debug Information', width: 100 },
+                {field: 'faultMessage', displayName: 'Fault Message', width: 100, visible: true},
+                {field: 'faultCode', displayName: 'Fault Code', width: 100},
+                {field: 'faultType', displayName: 'Fault Type', width: 100},
+                {field: 'severity', displayName: 'Severity', width: 100},
+                {field: 'correlationId', displayName: 'Correlation ID', width: 100},
+                {field: 'detail', displayName: 'Details'}
+            ],
+            selectedItems: $scope.mySelections,
+            multiSelect: false
         };
+        //https://github.com/angular-ui/ng-grid/wiki/Defining-columns
         //http://angular-ui.github.io/ng-grid/
 
 
